cedbca6351aacedcd3c20a9ad69d0ca8fab836aa
Andy Ho <155758784+AndyHo04@users.noreply.github.com>
2024-07-31 10:55:05 -0400

    done

--- W3/se251pong_w3_start_files/js/options.js
+++ W3/se251pong_w3_start_files/js/options.js
@@ -14,6 +14,26 @@ options.addEventListener(`click`, e =>document.querySelector(`.sides`).classList
         . Show the fill's hex code in the output div 
 
 -----------*/
+var fill = document.querySelectorAll(`.fill`);
+fill.forEach((input, index) => {
+    // Set each input's value to equal the player's fill property
+    input.value = player[index].fill;
+    
+    // Set the output div's inner HTML to equal the player's fill property
+    input.nextElementSibling.innerHTML = player[index].fill;
+    
+    // Add an `input` event to the fill input
+    input.addEventListener(`input`, e => {
+        // Change the appropriate player's fill color so that it uses the fill input's value
+        player[index].fill = e.target.value;
+        
+        // Set the appropriate pad's fill to equal the player's fill
+        player[index].pad.fill = player[index].fill;
+        
+        // Update the output to display the appropriate player's fill
+        input.nextElementSibling.innerHTML = player[index].fill;
+    });
+});
 
 /*---------
     Program the six key inputs to do the following:
